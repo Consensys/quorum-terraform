887d4be9d3c0048ce318417392c1af0ec3329e93
Eric Lin <eric.lin@consensys.net>
2022-02-24 13:19:28 +1000

    change to private internal ips for bootnode
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- besu/aws/modules/besu_node/outputs.tf
+++ besu/aws/modules/besu_node/outputs.tf
@@ -1,3 +1,3 @@
 output "bootnode_ip" {
-  value = var.node_details["node_type"] == "bootnode" ? aws_instance.nodes[0].public_ip : null
+  value = var.node_details["node_type"] == "bootnode" ? aws_instance.nodes[0].private_ip : null
 }
